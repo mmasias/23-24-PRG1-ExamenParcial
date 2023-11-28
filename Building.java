804c3df43ad64122d0b51be0eff77ccc07433764
manuel@GNU.linux.casa <manuel@masiasweb.com>
2023-11-28 22:19:06 +0100

    Source code template

---
+++ Building.java
@@ -0,0 +1,44 @@
+public class Building {
+    public static void main(String[] args) {
+        final String WINDOW_CLOSED = ":[ ]:";
+        final String WINDOW_DAMAGED = ":[X]:";
+        final String WINDOW_UNDER_MAINTENANCE = ":[#]:";
+        final String WINDOW_OPEN_ON = ":[*]:";
+        final String WINDOW_OPEN_OFF = ":[º]:";
+        final String CENTRAL_GAP = "[    ]";
+
+    }
+
+    static void printCeiling() {
+        final String TIP = "     ".repeat(3) + "__/\\__";
+        final String HEADER = "|####|";
+        final String ANTENNA = "  |  ";
+        final String ROOF = "=".repeat(36);
+
+        System.out.println(TIP);
+        System.out.println(ANTENNA.repeat(3) + HEADER + ANTENNA.repeat(3));
+        System.out.println(ROOF);
+    }
+
+    static void printBasement() {
+        System.out.println("""
+                :[       ]::::|      |::::[       ]:
+                ------------------------------------
+                     ==========================
+                   ==============================
+                 ==================================
+                """);
+    }
+
+    static void cleanScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+
+    static void pause(int seconds) {
+        try {
+            Thread.sleep(1000 * seconds);
+        } catch (InterruptedException e) {
+        }
+    }
+}
