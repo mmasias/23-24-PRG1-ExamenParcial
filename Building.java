c33b060397c71f1b471a6f5795df850b24fe5258
Manuel Masías <mmasias@users.noreply.github.com>
2023-11-28 23:01:45 +0100

    Update Building.java

--- Building.java
+++ Building.java
@@ -30,7 +30,7 @@ public class Building {
                 """);
     }
 
-    static void cleanScreen() {
+    static void clearScreen() {
         System.out.print("\033[H\033[2J");
         System.out.flush();
     }
